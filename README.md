e4e1ff6f726458f4c095f2d38a67f38319dc3eef
Harry Scells <harryscells@gmail.com>
2023-05-30 16:26:29 +0200

    Add setup script.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # PyLucene (packaged)
 
-This repo automatically packages [PyLucene](https://lucene.apache.org/pylucene/) for [PyPI](https://pypi.org).
+This repo automatically packages [PyLucene](https://lucene.apache.org/pylucene/) so it can be pip-installable!
 
-Since the PyLucene project does not provide pre-built binaries, this repo builds them for you.
+## Installation
+
+Since it is [not possible](https://921kiyo.com/python-packaging-edge-cases/) (
+seriously!) to package a python module that depends on local wheel dependencies and packages installed from PyPI cannot
+depend on packages which are not also hosted on PyPI, pylucene can only be installed from this repository:
+
+```bash
+pip install git+https://github.com/hscells/pylucene-packaged.git
+```
+
+Essentially, this repo acts as a dummy wrapper which installs the correct version of pylucene for your platform as its
+only dependency.
+
+## Supported Platforms
+
+See: [dist/](dist/) for a list of supported platforms.
--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
-from distutils.core import setup
+import platform
+import sys
+
+from setuptools import setup, find_namespace_packages
+
+package_name = 'lucene'
+version = '9.4.1'
+python_tags = [f'cp{sys.version_info.major}{sys.version_info.minor}' for _ in range(2)]
+platform_name = platform.system().lower()
+platform_version = platform.release()
+
+# Super hacky mac stuff.
+if platform_name == "darwin":
+    platform_name = "macosx"
+    platform_version = platform.mac_ver()[0].split('.')[0] + '_0'
+
+architecture = platform.machine()
+wheel_file_name = f'{package_name}-{version}-{"-".join(python_tags)}-{platform_name}_{platform_version}_{architecture}.whl'
 
 setup(
-    name='lucene',
-    version='9.4.1',
+    name='pylucene-packaged',
+    version='2023.05.30.0',
     license='Apache License 2.0',
     url='https://github.com/hscells/pylucene-packaged',
+    packages=find_namespace_packages(),
+    install_requires=[f'lucene@https://github.com/hscells/pylucene-packaged/raw/main/dist/{wheel_file_name}'],
 )
