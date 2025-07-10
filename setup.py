be1d230c84604de04c5049b9d83fc88d4b2e5ffb
Harry Scells <hscells@users.noreply.github.com>
2025-07-10 15:30:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_namespace_packages
 
 package_name = 'lucene'
-version = '9.12.0'
+version = '10.10.0'
 python_tags = [f'cp{sys.version_info.major}{sys.version_info.minor}' for _ in range(2)]
 platform_name = platform.system().lower()
 platform_version = platform.release()
@@ -13,13 +13,14 @@ platform_version = platform.release()
 if platform_name == "darwin":
     platform_name = "macosx"
     platform_version = platform.mac_ver()[0].split('.')[0] + '_0'
+    architecture = "universal"
 
 architecture = platform.machine()
 wheel_file_name = f'{package_name}-{version}-{"-".join(python_tags)}-{platform_name}_{platform_version}_{architecture}.whl'
 
 setup(
     name='pylucene-packaged',
-    version='2025.07.10.0',
+    version='2025.07.10.1',
     license='Apache License 2.0',
     url='https://github.com/hscells/pylucene-packaged',
     packages=find_namespace_packages(),
